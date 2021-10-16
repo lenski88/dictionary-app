a61957bc9a89afeef8a55b92464ba451fa33b328
lenski88 <lenski88@icloud.com>
2021-10-16 21:56:04 +0300

    feat: add preloader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import { Search } from './components/Search/Search';
+import { Preloader } from './components/Preloader/Preloader';
+import { useSelector } from 'react-redux';
+
 import './App.css';
 
+
 function App() {
+  const state = useSelector(state => state.data)
   return (
     <div className="App">
       <Search/>
+     {state.status === 1 && <Preloader/>}
     </div>
   );
 }
---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Preloader.scss";
+
+export const Preloader = () => {
+  return (
+    <div className="preloader-container">
+      <div className="preloader"></div>
+    </div>
+  );
+};
