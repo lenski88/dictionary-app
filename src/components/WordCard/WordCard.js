2e0e37df19d3840169634e5517d76012ee8af750
lenski88 <lenski88@icloud.com>
2021-10-17 20:47:38 +0300

    feat: add word in the dictionary

--- src/components/WordCard/WordCard.js
+++ src/components/WordCard/WordCard.js
@@ -4,7 +4,31 @@ import "./WordCard.scss";
 
 export const WordCard = () => {
   const state = useSelector((state) => state.data);
-  const handlerAddWord = () => {};
+  const handlerAddWord = () => {
+    let dictionary = JSON.parse(localStorage.dictionary);
+    let isWord = dictionary.some((i) => {
+      return i.word === state.data[0].word;
+    });
+    if (isWord) {
+      return;
+    } else {
+      let lastId;
+      if (!dictionary.length) {
+        lastId = 0;
+      } else {
+        lastId = dictionary[dictionary.length - 1].id + 1;
+      }
+      let newWord = {
+        id: lastId,
+        word: state.data[0].word,
+        meanings: state.data[0].meanings[0].definitions[0].definition,
+      };
+      let finalDictionary = [...dictionary];
+      finalDictionary.push(newWord);
+      finalDictionary = JSON.stringify(finalDictionary);
+      localStorage.setItem("dictionary", finalDictionary);
+    }
+  };
   const card = state.data.map((i) => {
     return (
       <div key={i} className="word-card">
